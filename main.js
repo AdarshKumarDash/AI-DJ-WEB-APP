8ac1aacac7d3f874add9356a418f52b195b9291f
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-06-03 19:34:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 music = "";
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
 
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on("pose", gotposes);
 }
 
 function preload() {
@@ -21,4 +27,20 @@ function play() {
 
 function stop() {
     music.stop();
+}
+
+function modelloaded() {
+    console.log("posenet is initialized");
+}
+
+function gotposes(result) {
+    if (result.length > 0) {
+        console.log(result);
+        rightWristX = result[0].pose.rightWrist.x;
+        rightWristY = result[0].pose.rightWrist.y;
+        leftWristX = result[0].pose.leftWrist.x;
+        leftWristY = result[0].pose.leftWrist.y;
+        console.log("Left WristX: " + leftWristX + " Left WristY: " + leftWristY);
+        console.log("Right WirstX: " + rightWristX + " Right WristY: " + rightWristY);
+    }
 }
