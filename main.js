ed574266549ef24b0b08be69dc8f9e9b91939979
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-06-08 19:42:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ leftWristX = "";
 leftWristY = "";
 rightWristX = "";
 rightWristY = "";
+leftWristScore = "";
 
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(600, 500);
     video.hide();
     posenet = ml5.poseNet(video, modelloaded);
     posenet.on("pose", gotposes);
@@ -17,12 +19,9 @@ function preload() {
     music = loadSound("music.mp3");
 }
 
-function draw() {
-    image(video, 0, 0, 600, 500)
-}
-
 function play() {
     music.play();
+    music.setVolume(1);
 }
 
 function stop() {
@@ -42,5 +41,19 @@ function gotposes(result) {
         leftWristY = result[0].pose.leftWrist.y;
         console.log("Left WristX: " + leftWristX + " Left WristY: " + leftWristY);
         console.log("Right WirstX: " + rightWristX + " Right WristY: " + rightWristY);
+        leftWristScore = result[0].pose.keypoints[9].score;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    fill("orangered");
+    if (leftWristScore > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        number_lwy = Number(leftWristY);
+        floor_lwy = floor(number_lwy);
+        volume = floor_lwy / 500;
+        music.setVolume(volume);
+        document.getElementById("music_volume").innerHTML = volume;
     }
 }
